12ca003ebf45bd69fe397569cc82506cf4657dd1
oztuzelosman <oztuzelosman@gmail.com>
2023-03-18 21:14:58 +0300

    finish button component with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import Button from "./Button.js";
-
+//
+import { GiAbstract008 } from "react-icons/gi";
+import { GiAbstract016 } from "react-icons/gi";
+import { GiAbstract024 } from "react-icons/gi";
+import { GiAbstract032 } from "react-icons/gi";
+import { GiAbstract040 } from "react-icons/gi";
+//
 const handleClick = () => {
   console.log("clicked");
 };
@@ -7,19 +13,24 @@ const handleClick = () => {
 function App() {
   return (
     <div>
-      <Button primary rounded onClick={handleClick}>
+      <Button primary outline rounded onClick={handleClick}>
+        <GiAbstract008 />
         App
       </Button>
-      <Button secondary rounded onMouseEnter={handleClick}>
+      <Button secondary outline rounded onMouseEnter={handleClick}>
+        <GiAbstract016 />
         Mpp
       </Button>
-      <Button success rounded>
+      <Button success outline rounded>
+        <GiAbstract024 />
         Kpp
       </Button>
-      <Button wise rounded>
+      <Button wise outline rounded>
+        <GiAbstract032 />
         qpp
       </Button>
-      <Button danger rounded>
+      <Button danger outline rounded>
+        <GiAbstract040 />
         Tpp
       </Button>
     </div>
--- src/Button.js
+++ src/Button.js
@@ -11,20 +11,24 @@ function Button({
   rounded,
   ...rest
 }) {
-  const classes = classnames("flex-1 flex-row item-center px-3 py-1.5 border", {
-    "text-white border-blue-600 bg-blue-600": primary,
-    "text-white border-gray-600 bg-gray-600": secondary,
-    "text-white border-green-600 bg-green-600": success,
-    "text-white border-purple-600 bg-purple-600": wise,
-    "text-white border-red-600 bg-red-600": danger,
-    "bg-white": outline,
-    "text-blue-500": outline && primary,
-    "text-gray-500": outline && secondary,
-    "text-green-500": outline && success,
-    "text-purple-500": outline && wise,
-    "text-red-500": outline && danger,
-    "rounded-full": rounded,
-  });
+  const classes = classnames(
+    rest.className,
+    "flex flex-row items-center px-3 py-1.5 border",
+    {
+      "text-white border-blue-600 bg-blue-600": primary,
+      "text-white border-gray-600 bg-gray-600": secondary,
+      "text-white border-green-600 bg-green-600": success,
+      "text-white border-purple-600 bg-purple-600": wise,
+      "text-white border-red-600 bg-red-600": danger,
+      "bg-white": outline,
+      "text-blue-600": outline && primary,
+      "text-gray-600": outline && secondary,
+      "text-green-600": outline && success,
+      "text-purple-600": outline && wise,
+      "text-red-600": outline && danger,
+      "rounded-full": rounded,
+    }
+  );
   return (
     <button {...rest} className={classes}>
       {children}
